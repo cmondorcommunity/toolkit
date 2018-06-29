4f2560d64104fbce764669646b05af5fdaa09aee
Chris Mondor <chris.mondor@gmail.com>
2018-06-28 20:44:39 -0700

    fix providers, add comments, enable tty for interactive prompt

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,30 +1,41 @@
 #!/usr/bin/env bash
 
 TFENV="/usr/local/bin/tfenv"
+TF_INIT_PATH="/app/terraform/scripts/tf_init.sh"
 
 ${TFENV} install 0.11.7
 ${TFENV} use 0.11.7
 
+# Build TF Statefile s3 bucket
+# TODO test if exists
 echo "entering /app/terraform/src/00-init"
 cd /app/terraform/src/00-init
 
-echo "running ../scripts/tf_init.sh"
-exec ../scripts/tf_init.sh
+echo "running ${TF_INIT_PATH}"
+${TF_INIT_PATH}
 
-terraform apply
+#####
+terraform plan
+#####
 
+# Build VPC
 echo "entering /app/terraform/src/01-vpc"
 cd /app/terraform/src/01-vpc
 
-echo "running ../scripts/tf_init.sh"
-exec ../scripts/tf_init.sh
+echo "running ${TF_INIT_PATH}"
+${TF_INIT_PATH}
 
-terraform apply
+#####
+terraform plan
+#####
 
+# Deploy ECS Jenkins
 echo "entering /app/terraform/src/02-main"
 cd /app/terraform/src/02-main
 
-echo "running ../scripts/tf_init.sh"
-exec ../scripts/tf_init.sh
+echo "running ${TF_INIT_PATH}"
+${TF_INIT_PATH}
 
-terraform apply
+#####
+terraform plan
+#####
